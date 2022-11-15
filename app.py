280361e8e23debf6fea3fa78066635f7093deab4
Sad_Smiley <danilvovikov@gmail.com>
2022-11-16 00:14:45 +0800

    -

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from config import client_id
+import qrcode
+import pyimgur
+
+app = Flask(__name__)
+api = Api(app)
+
+
+def getStaticQR(key, data):
+    # имя конечного файла
+    filename = "test.png"
+    # генерируем qr-код
+    img = qrcode.make(key + " " + data)
+    # сохраняем img в файл
+    img.save(filename)
+    return uploadToImg(filename)
+
+
+def uploadToImg(path):
+    id = client_id
+
+    im = pyimgur.Imgur(id)
+    upload_image = im.upload_image(path)
+    return upload_image.link
+
+class Quote(Resource):
+    def get(self, key, data):
+        return getStaticQR(key, data)
+
