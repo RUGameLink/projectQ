5ddbd79e63360fda36f438da4024f7805e34fa81
Sad_Smiley <danilvovikov@gmail.com>
2022-11-16 01:24:18 +0800

    test

--- generatorQR/app.py
+++ generatorQR/app.py
@@ -6,31 +6,14 @@ import pyimgur
 app = Flask(__name__)
 api = Api(app)
 
-
-def getStaticQR(key, data):
-    # имя конечного файла
-    filename = "test.png"
-    # генерируем qr-код
-    img = qrcode.make(key + " " + data)
-    # сохраняем img в файл
-    img.save(filename)
-    return uploadToImg(filename)
-
-
-def uploadToImg(path):
-    id = '2513abfb1d6eab5'
-
-    im = pyimgur.Imgur(id)
-    upload_image = im.upload_image(path)
-    return upload_image.link
-
 class Quote(Resource):
-    def get(self, data):
-        url = getStaticQR("key", data)
+    def get(self, key, data):
+        url = "getStaticQR(key, data)"
         response = {
-            "Key": "key",
+            "Key": key,
             "Data": data,
             "URL": url
         }
         return response
+
 
