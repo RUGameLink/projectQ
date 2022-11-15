7d31ad3b43c7422348c6457d067896786166044a
Sad_Smiley <danilvovikov@gmail.com>
2022-11-15 21:41:31 +0800

    Хранилище для картинок

--- generatorQR/main.py
+++ generatorQR/main.py
@@ -1,10 +1,15 @@
 import qrcode
 
-# пример данных
-data = "Пiхуй, пляшем! Плевать, вальсируем!"
-# имя конечного файла
-filename = "test.png"
-# генерируем qr-код
-img = qrcode.make(data)
-# сохраняем img в файл
-img.save(filename)
+def getStaticQR(key, data):
+    # имя конечного файла
+    filename = "test.png"
+    # генерируем qr-код
+    img = qrcode.make(key + " " + data)
+    # сохраняем img в файл
+    img.save(filename)
+
+def main():
+    getStaticQR("1", "text")
+
+if __name__ == '__main__':
+    main()
