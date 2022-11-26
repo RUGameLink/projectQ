2f83b5e62e15c80975f99f3770ceedbc0c4890e2
Sad_Smiley <danilvovikov@gmail.com>
2022-11-26 12:50:12 +0800

    Обновление Api
    Обновил Api под новые реалии
    Докинул сырой документ с предметной областью

--- generatorQR/main.py
+++ generatorQR/main.py
@@ -1,40 +1,42 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource
 import qrcode
 import pyimgur
+import uuid
 
 app = Flask(__name__)
 api = Api()
 
 class Main(Resource):
-    def get(self, key, data):
-        url = getStaticQR(key, data)
+    def get(self):
+        key, url = getStaticQR()
         response = {
             "Key": key,
-            "Data": data,
             "URL": url
         }
         return response
 
-api.add_resource(Main, "/api/<key>&&<data>")
+api.add_resource(Main, "/api/getqr")
 api.init_app(app)
 
-def getStaticQR(key, data):
+def getStaticQR():
+    #UUID Пользователя
+    key = str(uuid.uuid4())
     # имя конечного файла
-    filename = "test.png"
+    filename = f"{key}.png"
     # генерируем qr-код
-    img = qrcode.make(key + " " + data)
+    img = qrcode.make(key)
     # сохраняем img в файл
     img.save(filename)
-    return uploadToImg(filename)
-
+    url = uploadToImg(filename)
+    return key, url
 
 def uploadToImg(path):
-    id = '2513abfb1d6eab5'
+    id = '2513abfb1d6eab5' #id для Imgur
 
-    im = pyimgur.Imgur(id)
-    upload_image = im.upload_image(path)
-    return upload_image.link
+    im = pyimgur.Imgur(id) #Подключение к Imgur
+    upload_image = im.upload_image(path) #Загрузка файла
+    return upload_image.link #Отправка ссылки на файл
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000, host="127.0.0.1")
