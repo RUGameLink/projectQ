e8930336fc7396e5e85896c8d92423357c542dde
Sad_Smiley <danilvovikov@gmail.com>
2023-02-16 18:17:37 +0800

    Небольшая отладка кода и верстки

--- mobile/event-system-app/app/src/main/java/com/example/event_system_app/Adapter/MyEventAdapter.kt
+++ mobile/event-system-app/app/src/main/java/com/example/event_system_app/Adapter/MyEventAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.event_system_app.Adapter
 
 import android.content.Context
+import android.text.TextUtils.split
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -29,8 +30,10 @@ class MyEventAdapter(private val events: ArrayList<Event>, private val context: 
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) { //Запись в айтем данных в заготовленные textview
-
-        holder.titleText.text = events[position].title
+        var title = split(events[position].title, 25)
+        holder.titleText.text = title[0] + "..."
+//        var title = events[position].title
+//        holder.titleText.text = events[position].title
         holder.tagText.text = events[position].tags
         holder.dateText.text = events[position].date
 
@@ -43,6 +46,16 @@ class MyEventAdapter(private val events: ArrayList<Event>, private val context: 
             .load(imgUrl)
             .placeholder(R.drawable.events_icon)
             .into(holder.eventSmallImg);
+    }
+
+    private fun split(s: String, size: Int): List<String> {
+        val chunks = mutableListOf<String>()
+        var i = 0
+        while (i < s.length) {
+            chunks.add(s.substring(i, Math.min(s.length, i + size)))
+            i += size
+        }
+        return chunks
     }
 
     override fun getItemCount(): Int {
