03f0e2ac1f62e09782b1f593085ba77206f26ae4
Sad_Smiley <danilvovikov@gmail.com>
2023-03-06 01:57:23 +0800

    Черновая Как будет, Заготовка под Панель редактирования мероприятия

---
+++ mobile/EventSystemResponsibleApp/app/src/main/java/com/example/eventsystemresponsibleapp/Activity/EventInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventsystemresponsibleapp.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventsystemresponsibleapp.R
+
+class EventInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_info)
+    }
+}
