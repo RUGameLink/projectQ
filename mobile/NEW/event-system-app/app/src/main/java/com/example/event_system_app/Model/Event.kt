0bbef3e9e0d79fc4cef3f2e0d3ec1b0ac5e2074f
Sad_Smiley <danilvovikov@gmail.com>
2023-04-14 19:04:09 +0800

    Активити Информации об Ивентах
    Новый драйвер для картинок
    Почти готовый сеттер инфы

--- mobile/NEW/event-system-app/app/src/main/java/com/example/event_system_app/Model/Event.kt
+++ mobile/NEW/event-system-app/app/src/main/java/com/example/event_system_app/Model/Event.kt
@@ -9,7 +9,7 @@ data class Event(
     val id: Long,
     val title: String,
     val description: String,
-    val imgUrl: String,
+    val imgUrl: Array<String>,
     val tags: String,
     val date: String,
     val location: String,
